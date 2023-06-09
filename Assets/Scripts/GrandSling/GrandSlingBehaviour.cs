d62839dd3e7a7c6ee585493f7d73fca9ecb1ed4a
EmreBeratKR <emreberatkrn@gmail.com>
2023-06-09 23:12:35 +0300

    grandsling model and animations

--- Assets/Scripts/GrandSling/GrandSlingBehaviour.cs
+++ Assets/Scripts/GrandSling/GrandSlingBehaviour.cs
@@ -7,14 +7,22 @@ namespace GrandSling
 {
     public class GrandSlingBehaviour : MonoBehaviour
     {
+        private static readonly int IsTalkingHash = Animator.StringToHash("isTalking");
+        
+        
+        [SerializeField] private Animator animator;
+
+
         public UnityAction<EventResponse> OnAttachedToHandle;
         public UnityAction<EventResponse> OnDetachedFromHandle;
-        
-        
-        
-        
-        
-        
+
+
+        public void SetIsTalking(bool value)
+        {
+            animator.SetBool(IsTalkingHash, value);
+        }
+
+
         [Serializable]
         public struct EventResponse
         {
