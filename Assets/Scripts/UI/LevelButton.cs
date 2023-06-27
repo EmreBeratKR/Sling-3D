21b4bd84b0c9491bbb6610bf3ebd963570b6ce7e
EmreBeratKR <emreberatkrn@gmail.com>
2023-06-27 15:49:17 +0300

    level map UI improved

--- Assets/Scripts/UI/LevelButton.cs
+++ Assets/Scripts/UI/LevelButton.cs
@@ -8,6 +8,13 @@ namespace UI
         [SerializeField] private Image icon;
 
 
+        private void Awake()
+        {
+            var shadow = icon.gameObject.AddComponent<Shadow>();
+            shadow.effectDistance = new Vector2(-10.5f, -10.5f);
+        }
+
+
         public void UpdateIcon(Sprite sprite)
         {
             icon.sprite = sprite;
