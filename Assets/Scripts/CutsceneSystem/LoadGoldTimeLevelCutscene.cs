290d3daa7c3933c6e5efba561098025bd240c47b
EmreBeratKR <emreberatkrn@gmail.com>
2023-06-27 14:10:11 +0300

    skip cutscene 11 if completed already

--- Assets/Scripts/CutsceneSystem/LoadGoldTimeLevelCutscene.cs
+++ Assets/Scripts/CutsceneSystem/LoadGoldTimeLevelCutscene.cs
@@ -6,10 +6,17 @@ namespace CutsceneSystem
     {
         private const int NormalTimeCutsceneIndex = 10;
         private const int GoldTimeCutsceneIndex = 11;
+        private const int AfterGoldTimeCutsceneIndex = 12;
 
 
         public void Load()
         {
+            if (TutorialManager.IsTutorialCompleted(GoldTimeCutsceneIndex))
+            {
+                SceneController.LoadCutScene(AfterGoldTimeCutsceneIndex);
+                return;
+            }
+            
             SceneController.LoadCutScene(GetCutsceneIndex());
         }
 
