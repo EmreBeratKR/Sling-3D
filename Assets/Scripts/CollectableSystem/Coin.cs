fc5f46b634aa488d6f2ac7e9ce6ca4f93339e8c7
EmreBeratKR <emreberatkrn@gmail.com>
2023-06-29 18:16:02 +0300

    coin collect sfx

--- Assets/Scripts/CollectableSystem/Coin.cs
+++ Assets/Scripts/CollectableSystem/Coin.cs
@@ -1,16 +1,33 @@
 using Goal_System;
 using Sling;
+using SoundSystem;
 using UnityEngine;
 
 namespace CollectableSystem
 {
     public class Coin : MonoBehaviour, ICollectable
     {
+        [SerializeField] private GameObject visual;
+        [SerializeField] private SfxPlayer collectSfx;
+
+
+        private Collider m_Collider;
+
+
+        private void Awake()
+        {
+            m_Collider = GetComponent<Collider>();
+        }
+
+
         public void Collect(SlingBehaviour collector)
         {
             FindObjectOfType<CoinGoal>().AddCoin(1);
             FindObjectOfType<CollectableSpawner>().Spawn(1);
-            Destroy(gameObject);
+            collectSfx.PlayRandomClip();
+            visual.SetActive(false);
+            m_Collider.enabled = false;
+            Destroy(gameObject, 1f);
         }
     }
 }
